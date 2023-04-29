1a8357c8e4829a55285bc966fa53bdc756e879ef
Garrett-Richards <garrett.richards22@students.bhasvic.ac.uk>
2023-04-29 10:59:17 +0100

    created a better user interface

--- src/main/MainConsole.java
+++ src/main/MainConsole.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Scanner;
 
 import generators.SpamGeneratorStats;
 
@@ -20,9 +21,32 @@ public class MainConsole {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //generate text and output
-        SGS.generateNew(100);
-        System.out.println(SGS.getNewGenerated());
+        Scanner inputScanner = new Scanner(System.in);
+        final int maxToGenerate = 9999;
+        int toGenerate = 1;
+        while(toGenerate != 0){
+            System.out.println("How many letters do you want generated? (type 0 to exit)");
+            String userInput = inputScanner.nextLine();
+            try {
+                toGenerate = Integer.parseInt(userInput);
+                if(toGenerate > maxToGenerate){
+                    System.out.println(String.format("Pick a number smaller than %d", maxToGenerate+1));
+                }
+                if(toGenerate < 0){
+                    System.out.println("Pick a positive number");
+                }
+            } catch (NumberFormatException e) {
+                toGenerate = maxToGenerate+1;
+                System.out.println(String.format("%s is not a number, try again", userInput));
+            }
+            if(toGenerate>0 && toGenerate <= maxToGenerate) {
+                //generate text and output
+                SGS.generateNew(toGenerate);
+                System.out.println(SGS.getNewGenerated());
+            }
+        }
+
+
     }
     private static File getFileFromResource(String fileName) throws URISyntaxException {
 
